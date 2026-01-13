f3b51cfb76bed715ed0bd26f127472f67238ab5f
John Doe <ryanelee00@gmail.com>
2026-01-12 18:45:09 -0800

    Removing older prebuilt button linkage and creating debug

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,7 @@ import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.revrobotics.spark.SparkFlex;
-import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -39,6 +39,8 @@ public class RobotContainer {
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
     private final SparkFlex intakeMotor = new SparkFlex(21, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+    private final TalonFX turretMotor = new TalonFX(20);
+
 
     public RobotContainer() {
         configureBindings();
@@ -63,17 +65,17 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+        //joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        //joystick.b().whileTrue(drivetrain.applyRequest(() ->
+        //    point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        //));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        //joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        //joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        //joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        //joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // Reset the field-centric heading on left bumper press.
         joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
@@ -82,6 +84,12 @@ public class RobotContainer {
 
         joystick.rightBumper().onTrue(Commands.runOnce(() -> intakeMotor.set(-0.5f)));
         joystick.rightBumper().onFalse(Commands.runOnce(() -> intakeMotor.set(0.0f)));
+
+        joystick.x().whileTrue(Commands.runOnce(() -> turretMotor.set(0.3)));
+        joystick.b().whileTrue(Commands.runOnce(() -> turretMotor.set(-0.3)));
+        joystick.x().onFalse(Commands.runOnce(() -> turretMotor.set(0)));
+        joystick.b().onFalse(Commands.runOnce(() -> turretMotor.set(0)));
+
     }
 
     public Command getAutonomousCommand() {
