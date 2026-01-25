6d1c235d34893a3c9e24dc93ed20434944baa334
rryann0 <151428854+rryann0@users.noreply.github.com>
2026-01-24 17:27:27 -0800

    hopper class & constants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+
+
+public final class Constants {
+    public static final class DriveConstants {}
+    public static final class ModuleConstants {}
+    public static final class AutoConstants {}
+    public static final class ClimberConstants {}
+    public static final class HopperConstants {
+        public static final int kHopperCanId = 25;
+    }
+    public static final class IntakeConstants {}
+    public static final class ShooterConstants {}
+    public static final class TurretConstants {}
+}
+
