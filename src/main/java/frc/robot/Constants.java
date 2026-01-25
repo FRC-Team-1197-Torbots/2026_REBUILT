6b2e55be72c39d49bae6c6fac3e88eedc46900eb
rryann0 <151428854+rryann0@users.noreply.github.com>
2026-01-24 17:35:26 -0800

    update constants class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,10 +8,17 @@ public final class Constants {
     public static final class AutoConstants {}
     public static final class ClimberConstants {}
     public static final class HopperConstants {
-        public static final int kHopperCanId = 25;
+        public static final int kHopperCanId = 24;
     }
     public static final class IntakeConstants {}
-    public static final class ShooterConstants {}
-    public static final class TurretConstants {}
+    public static final class ShooterConstants {
+        private final int ShooterCanId1 = 21;
+        private final int ShooterCanId2 = 22;
+        private final int HoodCanId = 23;
+    }
+    public static final class TurretConstants {
+        private final int TurretCanId = 20;
+        private final double p = 0.0012d;
+    }
 }
 
