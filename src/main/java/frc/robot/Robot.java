1e9463d58478d67b362f4f2de2d342b05939cfde
Richard Ioki <richie.ioki@gmail.com>
2026-02-11 19:13:35 -0800

    Adding laserCAN TCP

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import com.ctre.phoenix6.HootAutoReplay;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import au.grapplerobotics.*;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -21,6 +22,8 @@ public class Robot extends TimedRobot {
         .withJoystickReplay();
 
     public Robot() {
+        CanBridge.runTCP();
+
         m_robotContainer = new RobotContainer();
     }
 
