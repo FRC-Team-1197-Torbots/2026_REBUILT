ab1e10065bc21846142be746ae6d5bdc2d64f4fc
Sean O <156563872+Aidan-Kelley@users.noreply.github.com>
2026-03-19 00:16:59 -0700

    Adding shoot command

---
+++ src/main/java/frc/robot/Commands/ShootCommand.java
@@ -0,0 +1,13 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.ZoneDetection;
+
+public class ShootCommand extends Command {
+
+    public ShootCommand(Shooter leftshooter, Shooter rightshooter, Hopper hooper, ZoneDetection zd) {
+        
+    }    
+}
